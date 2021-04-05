17c6f833d424d44529804b6804cf4e0285c45d2e
yunis <yhuseynov19@gmail.com>
2021-04-05 17:45:35 +0300

    bot service

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'e))2wa9t(!0wh8@89mvi2!kj$tf%w#aliosqdgis$ov3s+kj!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["91.134.171.38"]
 
 # Celery Configuration Options
 CELERY_TIMEZONE = "Europe/Moscow"
@@ -139,7 +139,8 @@ STATIC_URL = '/static/'
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8080',
-    'http://127.0.0.1:8080'
+    'http://127.0.0.1:8080',
+    'http://91.134.171.38',
 ]
 
 # Настройки Бота
